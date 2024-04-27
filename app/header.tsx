20e8066d945240218f3e9b3636403e7178bfb6fe
kabom <naumce.simidzioski@gmail.com>
2024-04-27 20:17:13 +0200

    Setting up Next Auth

---
+++ app/header.tsx
@@ -0,0 +1,20 @@
+"use client"
+import { ModeToggle } from "@/components/mode-goggle"
+import { signIn, signOut, useSession } from "next-auth/react"
+import { Button } from "@/components/ui/button"
+export function Header() {
+  const session = useSession()
+
+  return (
+    <header>
+      <div>
+        {session.data ? (
+          <Button onClick={() => signOut()}>Sign out </Button>
+        ) : (
+          <Button onClick={() => signIn("google")}>Sign in</Button>
+        )}
+        <ModeToggle />
+      </div>
+    </header>
+  )
+}
